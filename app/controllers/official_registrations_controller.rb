3bc0aae6dc310c3dbc573bcc97142bd7919ceab7
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2021-11-24 13:14:24 +0530

    Added registration and login for official

---
+++ app/controllers/official_registrations_controller.rb
@@ -0,0 +1,23 @@
+class OfficialRegistrationsController < ApplicationController
+  layout "registration"
+
+  def new
+    @official = Official.new
+  end
+
+  def create
+    @official = Official.new(user_params)
+    if @official.save
+      session[:user_id] = @official.id
+      redirect_to root_path, flash: { success: "Account created successfully" }
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:official).permit(:name, :area, :username, :password, :password_confirmation)
+  end
+end
--- app/models/current.rb
+++ app/models/current.rb
@@ -1,3 +1,4 @@
 class Current < ActiveSupport::CurrentAttributes
   attribute :customer
+  attribute :official
 end
